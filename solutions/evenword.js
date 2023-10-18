e9cd8ff307dd98be6328b053cb749259c55f5d54
Christine <christinekhekama99@gmail.com>
2023-10-18 19:11:42 +0300

    even word js solution

--- solutions/evenword.js
+++ solutions/evenword.js
@@ -1,1 +1,24 @@
- 
+function solution(S) {
+    let letterCount = {};
+  
+    for (let letter of S) {
+      letterCount[letter] = (letterCount[letter] || 0) + 1;
+    }
+  
+    let deleteCount = 0;
+  
+    for (let letter in letterCount) {
+      if (letterCount[letter] % 2 !== 0) {
+        deleteCount++;
+      }
+    }
+  
+    return deleteCount;
+  }
+  
+  
+  console.log(solution("acbcbba")); 
+  console.log(solution("axxaxa")); 
+  console.log(solution("aaaa"));   
+  console.log(solution("aaddcdassxxc")); 
+  console.log(solution("aaddcdasxxc")); 
